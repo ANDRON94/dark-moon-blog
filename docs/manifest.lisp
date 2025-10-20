b66646dd38ba438589141bdd5695d09384c168d6
andron94 <makedonsky94@gmail.com>
2025-10-19 17:43:30 -0700

    Start blog from scratch.

--- docs/manifest.lisp
+++
@@ -1,7 +0,0 @@
-(:docstring-markup-format :scriba
- :systems (dark-moon-blog)
- :documents ((:title "dark-moon-blog Manual"
-              :authors ("Andrii Tymchuk")
-              :output-format (:type :multi-html
-                              :template :minima)
-              :sources ("manual.scr"))))
--- src/routes/package.lisp
+++
@@ -1,8 +0,0 @@
-;;;; Define package for routes
-
-(in-package #:cl-user)
-
-(defpackage #:dark-moon-blog.routes
-  (:documentation "Define responds(views & api).")
-  (:use #:cl #:lucerne)
-  (:import-from #:dark-moon-blog #:app))
